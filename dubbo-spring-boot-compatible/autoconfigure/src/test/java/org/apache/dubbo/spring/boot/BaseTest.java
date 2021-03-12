8b2998e761ed731cb3e932f0351b17b980345a07
Albumen <jhq0812@gmail.com>
2021-03-12 20:40:04 +0800

    remove unused import

--- dubbo-spring-boot-compatible/autoconfigure/src/test/java/org/apache/dubbo/spring/boot/BaseTest.java
+++ dubbo-spring-boot-compatible/autoconfigure/src/test/java/org/apache/dubbo/spring/boot/BaseTest.java
@@ -1,10 +1,7 @@
 package org.apache.dubbo.spring.boot;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Albumen
